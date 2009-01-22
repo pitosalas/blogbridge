8efd2d5e67530cc56e89325e6de40845793b0759
Aleksey Gureiev <a.gureiev@noizeramp.com>
2009-01-22 14:31:05 +0200

    Shortening links from the tweet this dialog

--- src/com/salas/bb/twitter/TweetThisDialog.java
+++ src/com/salas/bb/twitter/TweetThisDialog.java
@@ -27,12 +27,16 @@ package com.salas.bb.twitter;
 import com.jgoodies.uif.AbstractDialog;
 import com.jgoodies.uif.util.ResourceUtils;
 import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.factories.ButtonBarFactory;
 import com.salas.bb.utils.i18n.Strings;
 import com.salas.bb.utils.uif.BBFormBuilder;
+import com.salas.bb.utils.net.LinkShortener;
+import com.salas.bb.utils.net.LinkShorteningException;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 /**
  * Tweet This dialog box.
@@ -62,8 +66,21 @@ public class TweetThisDialog extends AbstractDialog
     {
         JPanel content = new JPanel(new BorderLayout());
         content.add(buildBody(), BorderLayout.CENTER);
-        content.add(buildButtonBarWithOKCancel(), BorderLayout.SOUTH);
+        content.add(buildButtonBar(), BorderLayout.SOUTH);
         return content;
+    }
+
+    /**
+     * Creates buttons bar.
+     *
+     * @return bar.
+     */
+    private Component buildButtonBar()
+    {
+        JButton btnSend = createOKButton(true);
+        btnSend.setText(Strings.message("tweetthis.send"));
+
+        return ButtonBarFactory.buildOKCancelBar(btnSend, createCancelButton());
     }
 
     /**
@@ -85,7 +102,8 @@ public class TweetThisDialog extends AbstractDialog
 
         builder.append(pb.getPanel(), 1, CellConstraints.LEFT, CellConstraints.TOP);
         builder.append(taMessage, 3);
-        builder.append(Strings.message("tweetthis.shorten"), tfLink, btnShorten);
+        builder.append(Strings.message("tweetthis.shorten.link"), tfLink, btnShorten);
+        builder.appendUnrelatedComponentsGapRow();
 
         return builder.getPanel();
     }
@@ -97,7 +115,7 @@ public class TweetThisDialog extends AbstractDialog
     {
         tfLink     = new JTextField();
         taMessage  = new JTextArea(5, 70);
-        btnShorten = new JButton(Strings.message("tweetthis.shorten.btn"));
+        btnShorten = new JButton(new ShortenAction());
 
         Border spacing = BorderFactory.createLineBorder(new JLabel().getBackground(), 3);
 
@@ -105,6 +123,80 @@ public class TweetThisDialog extends AbstractDialog
         taMessage.setLineWrap(true);
         taMessage.setDocument(new TwitterMessage());
         taMessage.setBorder(BorderFactory.createCompoundBorder(spacing, tfLink.getBorder()));
+    }
+
+    /**
+     * Invoked when shortening should be performed.
+     */
+    private void onShorten()
+    {
+        componentsEnabled(false);
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
+                    String link = LinkShortener.process(tfLink.getText());
+                    onShortenSuccess(link);
+                } catch (LinkShorteningException e)
+                {
+                    onShortenFailure(e.getMessage());
+                } finally
+                {
+                    componentsEnabled(true);
+                }
+            }
+        });
+    }
+
+    /**
+     * Enables / disables related components.
+     *
+     * @param enabled TRUE to enable.
+     */
+    private void componentsEnabled(boolean enabled)
+    {
+        btnShorten.setEnabled(enabled);
+        tfLink.setEnabled(enabled);
+    }
+
+    /**
+     * Invoked when completed successfully.
+     *
+     * @param link link.
+     */
+    private void onShortenSuccess(String link)
+    {
+        tfLink.setText(link);
+    }
+
+    /**
+     * Invoked when completed with an error.
+     *
+     * @param error error.
+     */
+    private void onShortenFailure(String error)
+    {
+        JOptionPane.showMessageDialog(this, error,
+            Strings.message("tweetthis.dialog.title") + " - " + Strings.message("tweetthis.shorten.btn"),
+            JOptionPane.WARNING_MESSAGE);
+    }
+
+    /**
+     * Shorten link action.
+     */
+    private class ShortenAction extends AbstractAction
+    {
+        public ShortenAction()
+        {
+            super(Strings.message("tweetthis.shorten.btn"));
+        }
+
+        public void actionPerformed(ActionEvent e)
+        {
+            onShorten();
+        }
     }
 
     public static void main(String[] args)
